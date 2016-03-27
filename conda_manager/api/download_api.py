e49508c82938f2f95a3ba4a4ceeed77978c0e3da
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2016-03-27 03:06:32 -0500

    Py3 fixes, more error handling

--- conda_manager/api/download_api.py
+++ conda_manager/api/download_api.py
@@ -24,6 +24,19 @@ PY2 = sys.version[0] == '2'
 PY3 = sys.version[0] == '3'
 
 
+def to_binary_string(obj, encoding=None):
+    """Convert `obj` to binary string (bytes in Python 3, str in Python 2)"""
+    if PY2:
+        # Python 2
+        if encoding is None:
+            return str(obj)
+        else:
+            return obj.encode(encoding)
+    else:
+        # Python 3
+        return bytes(obj, 'utf-8' if encoding is None else encoding)
+
+
 def to_text_string(obj, encoding=None):
     """Convert `obj` to (unicode) text string."""
     if PY2:
@@ -54,14 +67,20 @@ def handle_qbytearray(obj, encoding):
 
 
 class DownloadWorker(QObject):
-    sig_download_finished = Signal(str, str)            # url, path
-    sig_download_progress = Signal(str, str, int, int)  # url, path, progress_size, total_size
+    # url, path
+    sig_download_finished = Signal(str, str)
+    # url, path, progress_size, total_size
+    sig_download_progress = Signal(str, str, int, int)
     sig_finished = Signal(object, object, object)
 
     def __init__(self, url, path):
         super(DownloadWorker, self).__init__()
         self.url = url
         self.path = path
+        self.finished = False
+
+    def is_finished(self):
+        return self.finished
 
 
 class _DownloadAPI(QObject):
@@ -73,27 +92,52 @@ class _DownloadAPI(QObject):
         self._chunk_size = chunk_size
         self._head_requests = {}
         self._get_requests = {}
-        self._data = {}
+        self._paths = {}
         self._workers = {}
-        self._old_requests = []
+#        self._old_requests = {}
 
         self._manager = QNetworkAccessManager(self)
+        self._timer = QTimer()
 
+        # Setup
+        self._timer.setInterval(1000)
+        self._timer.timeout.connect(self._clean)
+
+        # Signals
         self._manager.finished.connect(self._request_finished)
+        self._manager.sslErrors.connect(self._handle_ssl_errors)
+
+    def _handle_ssl_errors(self, reply, errors):
+        logger.error(str(('SSL Errors', errors)))
+
+    def _clean(self):
+        """
+        Periodically check for inactive workers and remove their references.
+        """
+        if self._workers:
+            for url in self._workers.copy():
+                w = self._workers[url]
+                if w.is_finished():
+                    self._workers.pop(url)
+#                    self._old_requests.pop(url)
+                    self._paths.pop(url)
+                    if url in self._get_requests:
+                        self._get_requests.pop(url)
+
+        else:
+            self._timer.stop()
 
     def _request_finished(self, reply):
-        """
-        """
-        url = reply.url().toString()
+        url = to_text_string(reply.url().toEncoded(), encoding='utf-8')
 
-        if url in self._data:
-            path = self._data[url]
+        if url in self._paths:
+            path = self._paths[url]
         if url in self._workers:
             worker = self._workers[url]
 
         if url in self._head_requests:
             req = self._head_requests.pop(url)
-            self._old_requests.append(req)
+#            self._old_requests[url] = req
             start_download = True
             header_pairs = reply.rawHeaderPairs()
             headers = {}
@@ -106,21 +150,27 @@ class _DownloadAPI(QObject):
             # Check if file exists
             if os.path.isfile(path):
                 file_size = os.path.getsize(path)
-
                 # Check if existing file matches size of requested file
                 start_download = file_size != total_size
 
             if start_download:
+                # File sizes dont match, hence download file
                 qurl = QUrl(url)
                 request = QNetworkRequest(qurl)
                 self._get_requests[url] = request
                 reply = self._manager.get(request)
+
+                error = reply.error()
+                if error:
+                    logger.error(str(('Reply Error:', error)))
+
                 reply.downloadProgress.connect(
                     lambda r, t, w=worker: self._progress(r, t, w))
             else:
+                # File sizes match, dont download file
+                worker.finished = True
                 worker.sig_download_finished.emit(url, path)
                 worker.sig_finished.emit(worker, path, None)
-                self._workers.pop(url)
         elif url in self._get_requests:
             data = reply.readAll()
             self._save(url, path, data)
@@ -129,17 +179,22 @@ class _DownloadAPI(QObject):
         """
         """
         worker = self._workers[url]
-        path = self._data[url]
-        with open(path, 'wb') as f:
-            f.write(data)
+        path = self._paths[url]
+
+        if not os.path.isfile(path):
+            #data = data
+            if len(data):
+                with open(path, 'wb') as f:
+                    f.write(data)
 
         # Clean up
+        worker.finished = True
         worker.sig_download_finished.emit(url, path)
         worker.sig_finished.emit(worker, path, None)
         req = self._get_requests.pop(url)
-        self._old_requests.append(req)
+#        self._old_requests[url] = req
         self._workers.pop(url)
-        self._data.pop(url)
+        self._paths.pop(url)
 
     def _progress(self, bytes_received, bytes_total, worker):
         """
@@ -150,23 +205,28 @@ class _DownloadAPI(QObject):
     def download(self, url, path):
         """
         """
+        # original_url = url
+        qurl = QUrl(url)
+        url = to_text_string(qurl.toEncoded(), encoding='utf-8')
+
         logger.debug(str((url, path)))
-        worker = DownloadWorker(url, path)
         if url in self._workers:
-            return worker
+            while not self._workers[url].finished:
+                return self._workers[url]
+
+        worker = DownloadWorker(url, path)
 
         # Check download folder exists
         folder = os.path.dirname(os.path.abspath(path))
         if not os.path.isdir(folder):
             os.makedirs(folder)
 
-        qurl = QUrl(url)
-        url = qurl.toString()
         request = QNetworkRequest(qurl)
         self._head_requests[url] = request
-        self._data[url] = path
+        self._paths[url] = path
         self._workers[url] = worker
         self._manager.head(request)
+        self._timer.start()
 
         return worker
 
@@ -358,7 +418,8 @@ class _RequestsDownloadAPI(QObject):
         data = {}
         try:
             r = requests.get(url)
-            data = json.loads(r.content)
+            content = to_text_string(r.content, encoding='utf-8')
+            data = json.loads(content)
         except Exception as error:
             logger.error(str(error))
 

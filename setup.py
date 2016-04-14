a970bf08ee0f4219feb8cc2e0090fa3e758bc5a3
Carlos Cordoba <ccordoba12@gmail.com>
2016-04-13 22:13:15 -0500

    Setup.py: Add hyphen to entry point

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     long_description=get_readme(),
     entry_points={
         'gui_scripts': [
-            'condamanager = conda_manager.app.main:main'
+            'conda-manager = conda_manager.app.main:main'
         ]
     },
     classifiers=[
@@ -100,4 +100,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Topic :: Software Development :: Widget Sets'])
+        'Topic :: Software Development :: Widget Sets']
+)
+
